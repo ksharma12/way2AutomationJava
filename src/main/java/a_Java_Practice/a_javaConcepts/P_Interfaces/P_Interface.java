14325b68b49eabbe6a5f0f58f0ea713a903153dd
ksharma12 <keshavsharma.tu@gmail.com>
2023-04-07 17:29:40 +0530

    New files addde

---
+++ src/main/java/a_Java_Practice/a_javaConcepts/P_Interfaces/P_Interface.java
@@ -0,0 +1,7 @@
+package a_Java_Practice.a_javaConcepts.P_Interfaces;
+
+public interface P_Interface {
+    public void method_1();
+    public abstract void method_2();
+
+}
---
+++ src/main/java/a_Java_Practice/a_javaConcepts/P_Interfaces/Test.java
@@ -0,0 +1,13 @@
+package a_Java_Practice.a_javaConcepts.P_Interfaces;
+
+public class Test implements P_Interface{
+    @Override
+    public void method_1() {
+
+    }
+
+    @Override
+    public void method_2() {
+
+    }
+}
