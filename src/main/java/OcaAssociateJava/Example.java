01fbd16d5a96572e319e03350de9588470b03707
ksharma12 <keshavsharma.tu@gmail.com>
2023-04-13 17:19:38 +0530

    Commiting the classes

---
+++ src/main/java/OcaAssociateJava/Example.java
@@ -0,0 +1,11 @@
+package OcaAssociateJava;
+
+public class Example {
+    public static void main(String[] args) {}
+    public static void main1(String[] asdf) {}
+    public static void main2(String[]... args) {}
+    public static void main3(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
